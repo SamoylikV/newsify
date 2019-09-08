1917fcbb2ce964696f98b8d7adce2f4e34d83a1a
Toxlc <44618834+Toxlc@users.noreply.github.com>
2019-09-08 12:47:00 +0500

    Add files via upload
    Stable version

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import json
 import sys
 
 import requests
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 
@@ -19,20 +19,29 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.my_web = self.ui.brs
         self.ui.errors.setStyleSheet("color: rgb(255, 0, 0);")
+        self.ui.centralwidget.setStyleSheet("background-color: rgb(255,255,255);")
         self.ui.raion.setReadOnly(True)
+        self.ui.raion.setStyleSheet('''border-radius: 8px;background-color: rgb(200,200,200);''')
+        self.ui.list.setStyleSheet('''border-radius: 10px;background-color: rgb(200,200,200); color:rgb(26,26,26);''')
+        self.ui.find.setStyleSheet('''border-radius: 8px;background-color: rgb(200,200,200);''')
+        self.ui.filter.setStyleSheet('''border-radius: 8px;background-color: rgb(240,240,240);''')
+        # self.ui.filter.setStyleSheet("QComboBox{border-radius: 8px;background-color: rgb(255,255,255); color: rgb(255,255,255);}QComboBox QAbstractItemView{border: 8px;color:rgb(255,255,255);background-color: rgb(255,255,255);}")
         self.ui.find.setEnabled(False)
         self.ui.find.clicked.connect(self.find)
         self.combo = self.ui.filter
         self.combo.activated[str].connect(self.filters)
         self.acs = ''
+        self.ui.list.itemDoubleClicked.connect(self.listing)
         self.kat = ''
         self.fnd = ''
+        self.link = ['','','','','','','','','','','','','','']
+        self.text_x = ['','','','','','','','','','','','','','']
         self.my_web.load(QUrl(
-            "https://oauth.vk.com/authorize?client_id=7080257&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.101&scope=photos,audio,video,pages,wall,docs,groups,offset&response_type=token"))
+            "https://oauth.vk.com/authorize?client_id=7080257&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.101&scope=photos,audio,video,pages,wall,docs,groups,offline&revoke=1&response_type=token"))
         self.hash = ''
         self.tmp = ''
         self.town = ''
-        self.link = 'http://www.fort-dev.ml/newsify/frame.php/'
+        self.l =''
         self.ui.brs.urlChanged.connect(self.token)
 
     def token(self):
@@ -59,7 +68,7 @@ class mywindow(QtWidgets.QMainWindow):
                                     print(self.hash)
                                 print(self.tmp)
                                 if res_tmp1 != "PyQt5.QtCore.QUrl('https://m.vk.com/login?act=authcheck":
-                                    if res_tmp1 != "PyQt5.QtCore.QUrl('http://www.fort-dev.ml/newsify/wait.php')":
+                                    if res_tmp1 != "PyQt5.QtCore.QUrl('" + self.l + "')":
                                         if res_tmp1 != "PyQt5.QtCore.QUrl('http://www.fort-dev.ml/newsify/wait.php?i=1')":
                                             if res_tmp1 != "PyQt5.QtCore.QUrl('http://www.fort-dev.ml/newsify/wait.php?i=2')":
                                                 if res_tmp1 != "PyQt5.QtCore.QUrl('https://oauth.vk.com/authorize?client_id=7080257":
@@ -70,18 +79,24 @@ class mywindow(QtWidgets.QMainWindow):
                                                     self.ui.find.setEnabled(True)
                                                     self.ui.raion.setReadOnly(False)
                                                     print('self.acs=', self.acs)
-
-                                                    l = 'http://www.fort-dev.ml/newsify/wait.php' + self.hash
-                                                    self.my_web.load(QUrl(l))
+                                                    if self.hash == '':
+                                                        self.l = 'http://www.fort-dev.ml/newsify/wait2.php'
+                                                        self.my_web.load(QUrl(self.l))
+                                                    else:
+                                                        self.l = 'http://www.fort-dev.ml/newsify/wait.php&hash=' + self.hash
+                                                        self.my_web.load(QUrl(self.l))
 
     def filters(self):
-        print(self.combo.text())
-        if self.combo.text() != "Все":
-            self.kat = self.combo.text()
+        print(self.combo.currentText())
+        if self.combo.currentText() != "Все":
+            self.kat = self.combo.currentText()
         else:
             self.kat = ''
 
+    def listing(self):
+        self.my_web.load(QUrl(self.link[self.ui.list.currentRow()]))
     def find(self):
+        self.ui.list.clear()
         inf = 'https://api.vk.com/method/account.getProfileInfo?access_token=' + str(self.acs) + '&v=5.101'
         inf = requests.get(inf)
         resp = inf.text
@@ -106,51 +121,53 @@ class mywindow(QtWidgets.QMainWindow):
             if self.town != '':
                 self.ui.errors.setText("")
                 self.ui.raion.setText(self.town)
-                fn = 'https://api.vk.com/method/newsfeed.search?q=' + self.town + ' ' + self.kat + ' новости&count=100&access_token=' + Tokkens + '&v=5.101'
+                fn = 'https://api.vk.com/method/newsfeed.search?q=' + self.town + ' ' + self.kat + ' новости&count=14&access_token=' + Tokkens + '&v=5.101'
                 req = requests.get(fn)
                 self.fnd = fn
+                print(fn)
                 resp = req.text
                 dict = json.loads(resp)
                 dict = dict.get('response')
                 dict = dict.get('items')
                 i = 0
-                while i < 7:
+                while i < 14:
                     id_x = dict[i].get('id')
                     owner_id = dict[i].get('owner_id')
+                    self.text_x[i] = str(dict[i].get('text'))[0:100] + '...'
                     if owner_id == abs(owner_id):
                         lnk = 'https://vk.com/id' + str(owner_id) + '?w=wall' + str(owner_id) + '_' + str(id_x)
                     else:
                         lnk = 'https://vk.com/public' + str(abs(owner_id)) + '?w=wall' + str(owner_id) + '_' + str(id_x)
-                    self.link = self.link + 'l' + str(i + 1) + '=' + lnk + '&'
+                    self.link[i] = lnk
                     i = i + 1
                     print(lnk)
-                self.my_web.load(QUrl(self.link))
-                print(self.link)
+
+
         else:
             self.ui.errors.setText("")
             self.ui.raion.setText(self.town)
-            fn = 'https://api.vk.com/method/newsfeed.search?q=' + self.town + ' новости&count=100&access_token=' + Tokkens + '&v=5.101'
+            fn = 'https://api.vk.com/method/newsfeed.search?q=' + self.town +' '+ self.kat + ' новости&count=100&access_token=' + Tokkens + '&v=5.101'
             req = requests.get(fn)
             self.fnd = fn
             resp = req.text
+            print(fn)
             dict = json.loads(resp)
             dict = dict.get('response')
             dict = dict.get('items')
             i = 0
-            while i < 7:
+            while i < 14:
                 id_x = dict[i].get('id')
                 owner_id = dict[i].get('owner_id')
+                self.text_x[i] = str(dict[i].get('text'))[0:100] + '...'
                 if owner_id == abs(owner_id):
                     lnk = 'https://vk.com/id' + str(owner_id) + '?w=wall' + str(owner_id) + '_' + str(id_x)
                 else:
                     lnk = 'https://vk.com/public' + str(abs(owner_id)) + '?w=wall' + str(owner_id) + '_' + str(id_x)
-                self.link = self.link + 'l' + str(i + 1) + '=' + lnk + '&'
+                self.link[i] = lnk
                 i = i + 1
+
                 print(lnk)
-            self.my_web.load(QUrl(self.link))
-            print(self.link)
-
-
+        self.ui.list.addItems(self.text_x)
 app = QtWidgets.QApplication([])
 application = mywindow()
 application.show()
