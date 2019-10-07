877fd0f164b844fd2de5d0e1f3179b3a9dbfdc8d
Alexandr-Fox <al.rostilismith@yandex.ru>
2019-10-07 19:49:10 +0500

    изменено:      Main.py 	удалено:       Newsify.exe.spec 	изменено:      Newsify.py 	удалено:       __pycache__/Newsify.cpython-36.pyc 	удалено:       __pycache__/Newsify.cpython-37.pyc 	удалено:       __pycache__/kod.cpython-36.pyc 	удалено:       build/Newsify.exe/Analysis-00.toc 	удалено:       build/Newsify.exe/EXE-00.toc 	удалено:       build/Newsify.exe/Newsify.exe.exe.manifest 	удалено:       build/Newsify.exe/PKG-00.pkg 	удалено:       build/Newsify.exe/PKG-00.toc 	удалено:       build/Newsify.exe/PYZ-00.pyz 	удалено:       build/Newsify.exe/PYZ-00.toc 	удалено:       build/Newsify.exe/base_library.zip 	удалено:       build/Newsify.exe/warn-Newsify.exe.txt 	удалено:       build/Newsify.exe/xref-Newsify.exe.html

--- Main.py
+++ Main.py
@@ -24,10 +24,6 @@ class mywindow(QtWidgets.QMainWindow):
 		self.next = ''
 		self.old = ''
 		self.ui.countri.activated[str].connect(self.Countri)
-		self.ui.filter.setEnabled(True) 
-		self.ui.countri.setEnabled(True) 
-		self.ui.citi.setEnabled(True) 
-		self.ui.region.setEnabled(True) 
 		self.ui.region.activated[str].connect(self.Region)
 		self.ui.citi.activated[str].connect(self.Citi)
 		self.region_text = ['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','']
@@ -48,8 +44,6 @@ class mywindow(QtWidgets.QMainWindow):
 		self.fnd = ''  # инициализация переменной поискового запросы
 		self.link = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '','']  # инициализация массива ссылок
 		self.text_x = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '','']  # инициализация массива текста постов
-		self.my_web.load(QUrl(
-			"https://oauth.vk.com/authorize?client_id=7080257&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.101&scope=photos,audio,video,pages,wall,docs,groups,offline&revoke=1&response_type=token"))  # открытие страницы авторизации и запроса прав
 		self.town = ''  # инициализация переменной города
 		self.ui.brs.urlChanged.connect(self.token)
 		self.ui.else_1.setEnabled(False)
@@ -57,86 +51,83 @@ class mywindow(QtWidgets.QMainWindow):
 	def token(self):
 		tokken = str(self.my_web.url())  # получение данных из адресной строки
 		#tokken)
-		if tokken == "PyQt5.QtCore.QUrl('about:blank')":
-			print(tokken)
-		else:
-			if tokken == "PyQt5.QtCore.QUrl('https://oauth.vk.com/blank.html#error=access_denied&error_reason=user_denied&error_description=User denied your request')":
+		if tokken.split("#")[0] == "PyQt5.QtCore.QUrl('https://oauth.vk.com/blank.html":
+			tokken = tokken.split('#')[-1]  # обработка строки для получения токена
+			tokken = tokken.split('&', 1)[0]
+			self.acs = tokken.split('=')[-1]  # обработка строки и запись токена в переменную
+			self.town = 'https://api.vk.com/method/account.getProfileInfo?access_token=' + self.acs + '&v=5.101'  # запрос информации о пользователе
+
+			print(self.town)
+			self.town = requests.get(self.town)
+			self.town = self.town.text  # обработка json кода
+			self.town = json.loads(self.town)
+			#self.town)
+			self.town = self.town.get('response')
+			self.countrihome= self.town.get('country')
+			self.countrihome_id= self.countrihome.get('id')
+			self.countrihome_text= self.countrihome.get('title')
+			#self.town)
+			self.ui.find.setEnabled(True)  # разблокировка кнопки
+			#'self.acs=', self.acs)
+			self.countri = 'https://api.vk.com/method/database.getCountries?count=234&need_all=1&access_token=' + self.acs + '&v=5.101'
+			self.countri =  requests.get(self.countri)
+
+			self.countri = self.countri.text
+			self.countri = json.loads(self.countri)
+			self.countri = self.countri.get('response')
+			self.countri = self.countri.get('items')
+			self.ui.filter.setEnabled(True) 
+			self.ui.countri.setEnabled(True) 
+			self.ui.citi.setEnabled(True) 
+			self.ui.region.setEnabled(True) 
+			#self.countri)
+			i=0
+			while i<234:
+				self.countri_text[i] = self.countri[i].get('title')
+				#self.countri_text[i])
+				self.countri_id[i+1] = self.countri[i].get('id')
+				#self.countri_id[i])
+				i= i+1
+			self.countri_id[0] = self.countrihome_id
+			self.ui.countri.addItem(self.countrihome_text)
+			self.ui.countri.addItems(self.countri_text)
+			#self.city_text)
+			self.age = self.town.get('bdate')
+			self.age = str(self.age)
+			self.age = self.age.split('.')[-1]
+			#self.age)
+			self.age = int(self.age)
+			self.age = 2019 - self.age
+			#"age:",self.age)
+			self.town = self.town.get('home_town')  # извлечение данных о городе
+			#self.town)
+			self.hometown = str(self.town)
+			self.Countri()
+			#self.town)
+			if self.hometown != None:
+				self.ui.citi.addItem(self.hometown)
+				self.find()
+			else:
 				self.my_web.load(QUrl(
-					"https://oauth.vk.com/authorize?client_id=7080257&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.101&scope=photos,audio,video,pages,wall,docs,groups,offline&revoke=1&response_type=token"))  # если нажали выход загрузить страницу авторизации
-			elif tokken == "PyQt5.QtCore.QUrl('https://oauth.vk.com/')":
-				self.my_web.load(QUrl(
-					"https://oauth.vk.com/authorize?client_id=7080257&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.101&scope=photos,audio,video,pages,wall,docs,groups,offline&revoke=1&response_type=token"))  # если нажали выход загрузить страницу авторизации
-			elif tokken != "PyQt5.QtCore.QUrl('')":
-				inf = 'https://api.vk.com/method/account.getProfileInfo?access_token=' + str(self.acs) + '&v=5.101'
-				if tokken != inf:
-					if tokken != self.fnd:
-						if tokken != "PyQt5.QtCore.QUrl('http://www.fort-dev.ml/newsify/wait.php')":
-							print(self.fnd)
-							if self.fnd == '':
-								print(tokken)
-								tokken = tokken.split('#')[-1]  # обработка строки для получения токена
-								tokken = tokken.split('&', 1)[0]
-								print(tokken)
-								if tokken != "PyQt5.QtCore.QUrl('https://m.vk.com/login?act=authcheck" and tokken != "PyQt5.QtCore.QUrl('http://www.fort-dev.ml/newsify/wait.php')" and tokken != "PyQt5.QtCore.QUrl('http://www.fort-dev.ml/newsify/wait.php?i=1')" and tokken != "PyQt5.QtCore.QUrl('http://www.fort-dev.ml/newsify/wait.php?i=2')" and tokken != "PyQt5.QtCore.QUrl('http://www.fort-dev.ml/newsify/wait2.php')" and tokken != "PyQt5.QtCore.QUrl('https://oauth.vk.com/authorize?client_id=7080257" and tokken != "PyQt5.QtCore.QUrl('https://oauth.vk.com/oauth/authorize?client_id=7080257":
-															self.acs = tokken.split('=')[-1]  # обработка строки и запись токена в переменную
-															self.town = 'https://api.vk.com/method/account.getProfileInfo?access_token=' + self.acs + '&v=5.101'  # запрос информации о пользователе
-															print(self.town)
-															self.town = requests.get(self.town)
-															self.town = self.town.text  # обработка json кода
-															self.town = json.loads(self.town)
-															#self.town)
-															self.town = self.town.get('response')
-															self.countrihome= self.town.get('country')
-															self.countrihome_id= self.countrihome.get('id')
-															self.countrihome_text= self.countrihome.get('title')
-															print(self.countrihome_id)
-															print(self.countrihome_text)
-															#self.town)
-															self.ui.find.setEnabled(True)  # разблокировка кнопки
-															#'self.acs=', self.acs)
-															self.countri = 'https://api.vk.com/method/database.getCountries?count=234&need_all=1&access_token=' + self.acs + '&v=5.101'
-															self.countri =  requests.get(self.countri)
-															self.countri = self.countri.text
-															self.countri = json.loads(self.countri)
-															self.countri = self.countri.get('response')
-															self.countri = self.countri.get('items')
-															#self.countri)
-															i=0
-															while i<234:
-																self.countri_text[i] = self.countri[i].get('title')
-																#self.countri_text[i])
-																self.countri_id[i+1] = self.countri[i].get('id')
-																#self.countri_id[i])
-																i= i+1
-															self.countri_id[0] = self.countrihome_id
-															self.ui.countri.addItem(self.countrihome_text)
-															self.ui.countri.addItems(self.countri_text)
-															#self.city_text)
-															self.age = self.town.get('bdate')
-															self.age = str(self.age)
-															self.age = self.age.split('.')[-1]
-															#self.age)
-															self.age = int(self.age)
-															self.age = 2019 - self.age
-															#"age:",self.age)
-															self.town = self.town.get('home_town')  # извлечение данных о городе
-															#self.town)
-															self.hometown = str(self.town)
-															#self.town)
-															if self.hometown != None:
-																self.ui.citi.addItem(self.hometown)
-																self.find()
-															else:
-																self.my_web.load(QUrl(
-																	'http://www.fort-dev.ml/newsify/wait.php'))  # загрузка страницы ожидания
-															self.Countri()
+					'http://www.fort-dev.ml/newsify/wait.php'))  # загрузка страницы ожидания
+			self.Countri()
+		elif tokken == "PyQt5.QtCore.QUrl('https://oauth.vk.com/blank.html#error=access_denied&error_reason=user_denied&error_description=User denied your request')":
+			self.my_web.load(QUrl(
+				"https://oauth.vk.com/authorize?client_id=7080257&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.101&scope=photos,audio,video,pages,wall,docs,groups,offline&revoke=1&response_type=token"))  # если нажали выход загрузить страницу авторизации
+		elif tokken == "PyQt5.QtCore.QUrl('https://oauth.vk.com/')":
+			self.my_web.load(QUrl(
+				"https://oauth.vk.com/authorize?client_id=7080257&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.101&scope=photos,audio,video,pages,wall,docs,groups,offline&revoke=1&response_type=token"))  # если нажали выход загрузить страницу авторизации
+		elif tokken.split("wall")[0]!="PyQt5.QtCore.QUrl('https://m.vk.com/":
+			self.my_web.load(QUrl(
+					'http://www.fort-dev.ml/newsify/wait2.php'))  # загрузка страницы ожидания
+
 	def Countri(self):
 		i = self.ui.countri.currentIndex()
 		self.countri = self.countri_id[i]
 		self.ui.citi.addItem(self.hometown)
 		self.region_lnk = 'https://api.vk.com/method/database.getRegions?country_id='+str(self.countri)+'&count=1000&need_all=1&access_token=' + self.acs + '&v=5.101'
-		print(self.region_lnk)
 		self.region =  requests.get(self.region_lnk)
+		print(self.region_lnk)
 		self.region = self.region.text
 		self.region = json.loads(self.region)
 		self.region = self.region.get('response')
@@ -144,10 +135,11 @@ class mywindow(QtWidgets.QMainWindow):
 		self.region = self.region.get('items')
 		self.ui.region.clear()
 		i=0
+		self.ui.region.addItem("")
 		while i<counter:
 			self.region_text[i] = self.region[i].get('title')
 			#self.region_text[i])
-			self.region_id[i] = self.region[i].get('id')
+			self.region_id[i+1] = self.region[i].get('id')
 			#self.region_id[i])
 			self.ui.region.addItem(self.region_text[i])
 			i= i+1
@@ -210,39 +202,34 @@ class mywindow(QtWidgets.QMainWindow):
 
 	def listing(self):
 		print(self.link[self.ui.list.currentRow()])
-		print(self.ui.list.currentRow())
-		self.my_web.load(QUrl(
-			self.link[self.ui.list.currentRow()]))  # при двойном клике списка элемент открывает ссылку на этот пост
+		self.my_web.load(QUrl(self.link[self.ui.list.currentRow()]))  # при двойном клике списка элемент открывает ссылку на этот пост
 
 	def find1(self):
-		self.ui.errors.setText("")  # очистка поля ошибок
-		self.fnd = 'https://api.vk.com/method/execute.next?city=' + self.town + '&kategory='+self.kat+'&count=150&post=15&age='+str(self.age)+'&start_from=' + str(self.next) + '&access_token=' + self.acs + '&v=5.101' 
+		self.fnd = 'https://api.vk.com/method/execute.next?q=' + self.town + ' '+self.kat+'&count=150&post=15&age='+str(self.age)+'&start_from=' + str(self.next) + '&access_token=' + self.acs + '&v=5.101' 
 		self.Site()
 
 	def find(self):
 		self.town = self.ui.citi.currentText()  # запись данных в переменную из поля ввода
 		self.my_web.load(QUrl("http://www.fort-dev.ml/newsify/wait2.php"))  # отображение страницы "Выберите новость"
-		self.fnd = 'https://api.vk.com/method/execute.newsify?city=' + self.town + '&kategory=' + self.kat + '&count=150&post=15&age='+str(self.age)+'&access_token=' + self.acs + '&v=5.101' # создание ссылки поиска новостей 
+		self.fnd = 'https://api.vk.com/method/execute.newsify?q=' + self.town + ' ' + self.kat + '&count=150&post=15&age='+str(self.age)+'&access_token=' + self.acs + '&v=5.101' # создание ссылки поиска новостей 
 		self.Site()
 
 	def Site(self):
 		self.ui.list.clear()  # очистка списка
+		print(self.fnd)
 		self.my_web.load(QUrl("http://www.fort-dev.ml/newsify/wait2.php"))  # отображение страницы "Выберите новость"
 		req = requests.get(self.fnd)  # получение json кода
 		req = req.text
 		req = json.loads(req)
-		print(self.fnd)
+		print("----------")
 		req = req.get('response')
 		self.next = req.get('next_from')
-		print(type(self.next))
 		self.text_x = req.get('text')# извлечение новостей
 		self.link = req.get('lnk')  # запись ссылок в массив
 		if self.next == None:
 			self.ui.else_1.setEnabled(False)
 		else:
 			self.ui.else_1.setEnabled(True)
-			print(self.next)
-		print(self.link[0])
 		self.ui.list.addItems(self.text_x)  # добавление ссылок из масива в список
 
 
